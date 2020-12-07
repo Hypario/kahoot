71ce11e430c2b8c02ab4dd5486a63cad6aec5420
virgile@neckyw10.baraffles <virgile@neckyw10.baraffles>
2020-12-07 23:22:01 +0100

    j'avions oublié le client listener

---
+++ src/client/ClientListener.java
@@ -0,0 +1,44 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+import common.Message;
+
+public class ClientListener extends Thread{
+
+	Socket socket;
+	ObjectInputStream ois;
+	InputStream is;
+	
+	
+	public ClientListener(Socket soc)  {
+		socket = soc;
+	}
+	
+	public void run() {
+		System.out.println("Client Listener Thread Started");
+		while(!Thread.currentThread().isInterrupted()) {
+			Message msg;
+			try {
+				ois = new ObjectInputStream(socket.getInputStream());
+				while((msg = (Message) ois.readObject()) != null) {
+				ClientRunner.getInstance().RecieveServer(msg);	
+				}
+			} catch (IOException e) {
+				ClientFrame.getInstance().showError(e.getMessage(), "Erreur IO Thread Reader");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				ClientFrame.getInstance().showError(e.getMessage(), "Erreur Class Not Found");
+			}
+			
+		}
+	}
+	
+	public void close_listen() throws IOException {
+		ois.close();
+	}
+
+}
